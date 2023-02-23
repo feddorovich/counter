2fcdb14360b43e049417cfb4c1b8fa2fb570d5c2
feddorovich <feddorovich@outlook.com>
2023-02-23 12:46:07 +0300

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Button} from "./components/Button/Button";
 import {Settings} from "./components/Settings/Settings";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
 
     let [counter, setCounter] = useState<number>(0)
     let [settings, setSettings] = useState(false)
@@ -16,15 +16,17 @@ const App:React.FC = () => {
         if (counter < maxValue) {
             setError(false)
             setCounter(counter + 1)
-        } else {
+        }
+        if (counter === maxValue - 1) {
             setError(true)
         }
-
     }
+
     function resetCount() {
         setError(false)
         setCounter(0)
     }
+
     function openSettings() {
         setError(false)
         setSettings(!settings)
@@ -32,21 +34,23 @@ const App:React.FC = () => {
 
     return (
         <div className="App">
-            { settings ? <Settings openSettings={openSettings}
-                                   setCounter={setCounter}
-                                   maxValue={maxValue}
-                                   setMaxValue={setMaxValue}
-                                   minValue={minValue}
-                                   setMinValue={setMinValue}
+            {settings ? <Settings openSettings={openSettings}
+                                  setCounter={setCounter}
+                                  maxValue={maxValue}
+                                  setMaxValue={setMaxValue}
+                                  minValue={minValue}
+                                  setMinValue={setMinValue}
                 /> :
-            <div className={'counter'}>
-                <div className={error ? 'numArea' + ' ' + 'maxValue' : 'numArea'}>{counter}</div>
-                <div className={'buttoms'}>
-                    <Button title={'inc'} callback={addCount} disabled={error}/>
-                    <Button title={'reset'} callback={resetCount} disabled={counter===minValue}/>
-                    <Button title={'set'} callback={()=>{setSettings(true)}}/>
+                <div className={'counter'}>
+                    <div className={error ? 'numArea' + ' ' + 'maxValue' : 'numArea'}>{counter}</div>
+                    <div className={'buttoms'}>
+                        <Button title={'inc'} callback={addCount} disabled={error}/>
+                        <Button title={'reset'} callback={resetCount} disabled={counter === minValue}/>
+                        <Button title={'set'} callback={() => {
+                            setSettings(true)
+                        }}/>
+                    </div>
                 </div>
-            </div>
             }
         </div>
     );
