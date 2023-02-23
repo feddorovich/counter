68dea8bf694ab585619ac0ca4efae51db10f9c24
feddorovich <feddorovich@outlook.com>
2023-02-23 10:47:40 +0300

    settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button/Button";
+import {Settings} from "./components/Settings/Settings";
 
 function App() {
 
     let [counter, setCounter] = useState<number>(0)
+    let [settings, setSettings] = useState(false)
     let minValue = 0
     let maxValue = 5
 
@@ -18,14 +20,16 @@ function App() {
 
     return (
         <div className="App">
-            <div className={counter===maxValue ? 'counter' + ' ' + 'maxValue' : 'counter'}>
-                <div className={'numArea'}>{counter}</div>
+            { settings ? <Settings/> :
+            <div className={'counter'}>
+                <div className={counter===maxValue ? 'numArea' + ' ' + 'maxValue' : 'numArea'}>{counter}</div>
                 <div className={'buttoms'}>
                     <Button title={'inc'} callback={addCount} disabled={counter===maxValue}/>
                     <Button title={'reset'} callback={resetCount} disabled={counter===minValue}/>
-                    <Button title={'set'} callback={()=>{}} disabled={true}/>
+                    <Button title={'set'} callback={()=>{setSettings(true)}}/>
                 </div>
             </div>
+            }
         </div>
     );
 }
